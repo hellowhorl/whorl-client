1228c2db32bcf4af35621689ede1313485b9b9d2
dluman <dluman@allegheny.edu>
2024-10-14 06:48:17 -0400

    Splitting the API into a client and server repository

--- src/climate/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from climate.models import ClimateModel
-
-class ClimateModelSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = ClimateModel
-        fields = "__all__"
-
--- src/climate/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path, path
-from .views import ClimateDataViewAll
-
-urlpatterns = [
-    path('', ClimateDataViewAll.as_view(), name='climate-all'),
-]
