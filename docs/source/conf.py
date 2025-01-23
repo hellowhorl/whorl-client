a97b7adcc99828756e937448f0ee95baf516c4d5
Aidan Dyga <aidan.dyga@gmail.com>
2025-01-23 15:53:18 -0500

    fix: updating the version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'hello-whorl'
 copyright = '2025 hello-whorl'
 author = 'Preston Smith, Aidan Dyga, Rebekah Rudd, Jason Gyamafi'
-release = '1'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
