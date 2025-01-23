41f50b53b9ef9d1dcacbb1a83adf954852e19b2a
Rebekah Rudd <rudd01@allegheny.edu>
2025-01-23 16:07:43 -0500

    feat: add docstrings to Look.py

--- src/persona/Look.py
+++ src/persona/Look.py
@@ -1,18 +1,36 @@
 import os
 import sys
 import types
-import inspect
+# import inspect
 
 from rich.console import Console
 from rich.markdown import Markdown
 
 class Look:
 
+    """A class to look at the descriptions of objects.
+    
+    This class gets the description of objects and returns 
+    to the user what an object looks like.
+    
+    Attributes:
+        filename (str): Define a path to a file.
+    """
+
     def __init__(self, filename: str = ""):
         self.filename = filename
         self.__get_info()
 
     def __get_info(self):
+        """Query the persona API for active users in current directory.
+        
+        Returns:
+            None
+            
+        Raises:
+            A string that tells the user the file doesn't exist
+        """
+
         console = Console()
         mod = types.ModuleType(self.filename)
         try:
@@ -26,5 +44,17 @@ class Look:
         print()
 
 def cmd():
+    """Command-line entry point for Look functionality.
+    
+    Creates an instance of the Ego class which automatically access Look 
+    and see if there is an object description.
+    
+    Returns:
+        ??
+        
+    Example:
+        TODO
+    """
+
     sys.path.append(os.path.expanduser(os.getcwd()))
     Look(sys.argv[1])
