b0309b66ce721d75db6a30b7dbd43f520824a8b5
Rebekah Rudd <rudd01@allegheny.edu>
2025-02-13 15:42:35 -0500

    feat: basic Request outline

--- src/process_request.py
+++ src/process_request.py
@@ -4,6 +4,7 @@ import os
 import requests
 from dotenv import load_dotenv
 import sys
+from typing import Dict
 
 load_dotenv()
 
@@ -36,18 +37,64 @@ def process_request(request):
     data = request.get('data', {})
 
     # modify the response based on the web method
-    if method == 'GET':
-        response = requests.get(url, headers=headers, params=params)
-    elif method == 'POST':
-        response = requests.post(url, headers=headers, json=data)
-    elif method == 'PATCH':
-        response = requests.patch(url, headers=headers, json=data)
-    else:
-        raise ValueError(f"Unsupported HTTP method: {method}")
+    # if method == 'GET':
+    #     response = requests.get(url, headers=headers, params=params)
+    # elif method == 'POST':
+    #     response = requests.post(url, headers=headers, json=data)
+    # elif method == 'PATCH':
+    #     response = requests.patch(url, headers=headers, json=data)
+    # else:
+    #     raise ValueError(f"Unsupported HTTP method: {method}")
     
     # display nothing if a 403 is returned
-    if response.status_code == 403:
-        sys.exit(1) # exit the program and return nothing if the response is 403
+    # if response.status_code == 403:
+    #     sys.exit(1) # exit the program and return nothing if the response is 403
 
 
     return response
+
+
+class Request:
+"""Defining a request class which uses different methods to return different parameters to the server."""
+
+    def __init__(self, method: str, url: str, data: Dict);
+
+        self.method = str
+        self.url = str
+        self.data = {}
+
+    def __get(self):
+        response = requests.get(url, headers=headers, params=params)
+        return response
+
+    def __post(self):
+        response = requests.post(url, headers=headers, json=data)
+        return response
+
+    def __patch(self):
+        response = requests.patch(url, headers=headers, json=data)
+        return response
+    
+    def __delete(self):
+        return response
+
+    def __update(self):
+        return response
+
+    def call_method(self, method_name):
+        if method == 'GET':
+            return self.__get()
+        elif method == 'POST':
+            return self.__post()
+        elif method == 'PATCH':
+            return self.__patch()
+        elif method == 'DELETE':
+            return self.__delete()
+        elif method == 'UPDATE':
+            return self.__update()
+        # display nothing if a 403 is returned
+        elif response.status_code == 403:
+            sys.exit(1) # exit the program and return nothing if the response is 403
+
+        else:
+            return "Invalid method name"
